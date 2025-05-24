a6772c85cc8c19e075cbf62bdfbafa7d22ae6d72
Juan Diego <juandiegogg1801@gmail.com>
2025-05-24 15:38:14 -0500

    Analisis léxico terminado

--- App.py
+++ App.py
@@ -107,13 +107,13 @@ def procesar_cadena(cadena):
 
 def lexer(estado_final, lexema):
     if estado_final == "q1":
-        print(f"LEXEMA: '{lexema}' → Tipo: TAG_OPEN")
+        print(f"LEXEMA: '{lexema}' → Tipo: MENOR_QUE")
     elif estado_final == "q2":
-        print(f"LEXEMA: '{lexema}' → Tipo: PREGUNTA")
+        print(f"LEXEMA: '{lexema}' → Tipo: PREGUNTA_FIN")
     elif estado_final == "q4":
         print(f"LEXEMA: '{lexema}' → Tipo: PALABRA")
     elif estado_final == "q5":
-        print(f"LEXEMA: '{lexema}' → Tipo: EXCLAMACION")
+        print(f"LEXEMA: '{lexema}' → Tipo: EXCLAMACION_FIN")
     elif estado_final == "q6":
         print(f"LEXEMA: ' ' → Tipo: ESPACIO")
     elif estado_final == "q7":
@@ -129,7 +129,7 @@ def lexer(estado_final, lexema):
     elif estado_final == "q14":
         print(f"LEXEMA: '{lexema}' → Tipo: GUION")
     elif estado_final == "q15":
-        print(f"LEXEMA: '{lexema}' → Tipo: TAG_CLOSE")
+        print(f"LEXEMA: '{lexema}' → Tipo: MAYOR_QUE")
     elif estado_final == "q16":
         print(f"LEXEMA: '{lexema}' → Tipo: SLASH")
     elif estado_final == "q17":
@@ -138,12 +138,61 @@ def lexer(estado_final, lexema):
         print(f"LEXEMA: '{lexema}' → Tipo: PUNTO")
     elif estado_final == "q19":
         print(f"LEXEMA: '{lexema}' → Tipo: COMILLA_SIMPLE")
+    elif estado_final == "q20":
+        print(f"LEXEMA: '{lexema}' → Tipo: CORCHETE_INICIO")
+    elif estado_final == "q21":
+        print(f"LEXEMA: '{lexema}' → Tipo: PUNTO_COMA")
+    elif estado_final == "q22":
+        print(f"LEXEMA: '{lexema}' → Tipo: COMA")
+    elif estado_final == "q23":
+        print(f"LEXEMA: '{lexema}' → Tipo: CORCHETE_FIN")
+    elif estado_final == "q24":
+        print(f"LEXEMA: '{lexema}' → Tipo: NUMERAL")
+    elif estado_final == "q25":
+        print(f"LEXEMA: '{lexema}' → Tipo: AMPERSAND")
+    elif estado_final == "q26":
+        print(f"LEXEMA: '{lexema}' → Tipo: PORCENTAJE")
+    elif estado_final == "q27":
+        print(f"LEXEMA: '{lexema}' → Tipo: MAS")
+    elif estado_final == "q28":
+        print(f"LEXEMA: '{lexema}' → Tipo: ARROBA")
+    elif estado_final == "q29":
+        print(f"LEXEMA: '{lexema}' → Tipo: PIPE")
+    elif estado_final == "q30":
+        print(f"LEXEMA: '{lexema}' → Tipo: BACKSLASH")
+    elif estado_final == "q31":
+        print(f"LEXEMA: '{lexema}' → Tipo: CIRCUNFLEJO")
+    elif estado_final == "q32":
+        print(f"LEXEMA: '{lexema}' → Tipo: TILDE")
+    elif estado_final == "q33":
+        print(f"LEXEMA: '{lexema}' → Tipo: DOLAR")
+    elif estado_final == "q34":
+        print(f"LEXEMA: '{lexema}' → Tipo: PARENTESIS_INICIO")
+    elif estado_final == "q35":
+        print(f"LEXEMA: '{lexema}' → Tipo: PARENTESIS_FIN")
+    elif estado_final == "q36":
+        print(f"LEXEMA: '{lexema}' → Tipo: CORCHETE_RECT_INICIO")
+    elif estado_final == "q37":
+        print(f"LEXEMA: '{lexema}' → Tipo: CORCHETE_RECT_FIN")
+    elif estado_final == "q38":
+        print(f"LEXEMA: '{lexema}' → Tipo: GUION_BAJO")
+    elif estado_final == "q39":
+        print(f"LEXEMA: '{lexema}' → Tipo: ASTERISCO")
+    elif estado_final == "q40":
+        print(f"LEXEMA: '{lexema}' → Tipo: PREGUNTA_INICIO")
+    elif estado_final == "q41":
+        print(f"LEXEMA: '{lexema}' → Tipo: EXCLAMACION_INICIO")
     else:
         print(f"LEXEMA: '{lexema}' → Tipo: ERROR")
 
 # Autómata
-estados = {"q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19"}
-letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúÁÉÍÓÚüÜñÑ"
+estados = {"q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9",
+           "q10", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18",
+           "q19", "q20", "q21", "q22", "q23", "q24", "q25", "q26", "q27",
+           "q28", "q29", "q30","q31", "q32", "q33", "q34", "q35", "q36",
+           "q37", "q38", "q39", "q40", "q41"}
+letras = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZáéíóúÁÉÍÓÚ"
+          "üÜñÑ")
 numeros = "0123456789"
 transiciones = {}
 
@@ -177,9 +226,35 @@ transiciones[("q0", "/")] = "q16"
 transiciones[("q0", ":")] = "q17"
 transiciones[("q0", ".")] = "q18"
 transiciones[("q0", "'")] = "q19"
+transiciones[("q0", "{")] = "q20"
+transiciones[("q0", ";")] = "q21"
+transiciones[("q0", ",")] = "q22"
+transiciones[("q0", "}")] = "q23"
+transiciones[("q0", "#")] = "q24"
+transiciones[("q0", "&")] = "q25"
+transiciones[("q0", "%")] = "q26"
+transiciones[("q0", "+")] = "q27"
+transiciones[("q0", "@")] = "q28"
+transiciones[("q0", "|")] = "q29"
+transiciones[("q0", "\\")] = "q30"
+transiciones[("q0", "^")] = "q31"
+transiciones[("q0", "~")] = "q32"
+transiciones[("q0", "$")] = "q33"
+transiciones[("q0", "(")] = "q34"
+transiciones[("q0", ")")] = "q35"
+transiciones[("q0", "[")] = "q36"
+transiciones[("q0", "]")] = "q37"
+transiciones[("q0", "_")] = "q38"
+transiciones[("q0", "*")] = "q39"
+transiciones[("q0", "¿")] = "q40"
+transiciones[("q0", "¡")] = "q41"
 
 estado_inicial = "q0"
-estados_finales = {"q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18", "q19"}
+estados_finales = {"q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9",
+                   "q11", "q12", "q13", "q14", "q15", "q16", "q17", "q18",
+                   "q19", "q20", "q21", "q22", "q23", "q24", "q25", "q26",
+                   "q27", "q28", "q29", "q30","q31", "q32", "q33", "q34",
+                   "q35", "q36", "q37", "q38", "q39", "q40", "q41"}
 
 # Leer entrada
 with open("entrada.txt", "r", encoding="utf-8") as archivo:
